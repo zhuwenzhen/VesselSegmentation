a3ff27d7a69baee340e423cbc5a2124f85ed732e
LoyukiL <wujiaye1996@gmail.com>
2016-11-27 23:49:31 -0600

    finished fglcm, frlm

--- VesselSegmentation/VesselSegmentation/python/fglcm.py
+++ VesselSegmentation/VesselSegmentation/python/fglcm.py
@@ -2,32 +2,32 @@ import scipy.interpolate
 import math
 import numpy as np
 #scipy.interpolate.interp2d(x_coords, y_coords, img)
-def fglcm_check_point(img, x, y, d, degree, m, n, interp_func, angle_mode="rad"):
-    if(angle_mode == "deg"):
-        degree = math.radians(degree)
+#degree = [[1, 0], [1, 1], [0, 1], [-1, 1]]
+def fglcm_check_point(img, x, y, d, degree, m, n):
+    #if(angle_mode == "deg"):
+    #    degree = math.radians(degree)
     if(round(img[y][x]) == m):
-        new_x = x + d * cos(degree)
-        new_y = y + d * sin(degree)
-        if(round(interp_func(new_x, new_y)) == n):
+        new_x = x + (d * degree)[0]
+        new_y = y + (d * degree)[1]
+        if(round(img[new_y, new_x]) == n):
             return True
     return False
-def glcm(img, degree, d, interp_func, angle_mode="rad"):
+def glcm(img, degree, d):
     glc_mat = np.zeros(img.shape())
     for m in range(256):
         for n in range(256):
             for x in range(img.shape()[1] - d):
                 for y in range(img.shape()[0] - d):
-                    glc_mat[m][n] += fglcm_check_point(img, x, y, d, degree, m, n, interp_func, angle_mode)
+                    glc_mat[m][n] += fglcm_check_point(img, x, y, d, degree, m, n)
     return glc_mat
 
 def fglcm(img, d):
     x_coords = list(range(0, img.shape()[1]))
     y_coords = list(range(0, img.shape()[0]))
-    f = scipy.interpolate.interp2d(x_coords, y_coords, img)
-    degrees = [0, 45, 90, 135]
+    degrees = np.array([[1, 0], [1, 1], [0, 1], [-1, 1]])
     glc_mats = []
     for deg in degrees:
-        glc_mat = glcm(img, deg, d, f, angle_mode="deg")
+        glc_mat = glcm(img, deg, d)
         glc_mats.append(glc_mat)
     fglcm = sum(glc_mats) / len(glc_mats)
     #max_ele = max(max(row) for row in fglcm)
--- VesselSegmentation/VesselSegmentation/python/frlm.py
+++ VesselSegmentation/VesselSegmentation/python/frlm.py
@@ -1,17 +1,52 @@
 import numpy as np
-def frlm(img, degree, angle_mode="rad"):
-    frl_mat = np.zeros(img.shape())
-    if(angle_mode == "deg"):
-        degree = math.radians(degree)
-    x = 0
-    y = 0
-    new_x = 0
-    new_y = 0
-    run = 0
-    if img[new_y][new_x] == img[y][x]:
-        run+=1
-    #TODO: figure out the loop
-    frl_mat[img[y][x]][run] += 1
+#degree = [[1, 0], [1, 1], [0, 1], [-1, 1]]
 
-def compute_sre():
-    pass
+def frlm(img):
+    x_sz = img.shape()[1]
+    y_sz = img.shape()[0]
+    coord_mat = array([(x, y) for y in range(x_sz) for x in range(y_sz)]).reshape(img.shape())
+    runs_135 = np.array([coord_mat.diagonal(i) for i in range(-y_sz+1, x_sz)])
+    runs_45 = np.array([np.flipud(coord_mat).diagonal(i) for i in range(-y_sz+1, x_sz)])
+    runs_0 = coord_mat.copy()
+    runs_90 = coord_mat.transpose().copy()
+    all_runs = [runs_135, runs_45, runs_0, runs_90]
+    all_rlms = map(lambda runs:rlm(img, runs),all_runs)
+    return all_rlms
+
+def rlm(img, runs):
+    frl_mat = np.zeros(img.shape())
+    #if(angle_mode == "deg"):
+    #    degree = math.radians(degree)
+    #TODO: figure out the loop
+    # idx_y = 0
+    # while idx_y < img.shape()[0]:
+    #     new_y = idx_y
+    #     while new_x >= 0 && new_x < img.shape()[1] && new_y >= 0 && new_y < img.shape()[0]:
+    #         #compute new_x new_y
+    #         x = new_x
+    #         y = new_y
+    #         new_x = x + degree[0]
+    #         new_y = y + degree[1]
+    #         if img[new_y][new_x] == img[y][x]:
+    #             run+=1
+    #         else:
+    #             frl_mat[img[y][x]][run] += 1
+    #             run = 1
+    #     idx_y += 1
+    for single_run in runs:
+        x, y = single_run[0]
+        run = 0
+        for new_x, new_y in single_run:
+            if img[new_y][new_x] == img[y][x]:
+                run+=1
+            else:
+                frl_mat[img[y][x]][run] += 1
+                run = 1
+            x = new_x
+            y = new_y
+        frl_mat[img[y, x][run] += 1
+    return frl_mat
+def compute_sre(frl_mat):
+    sre_up = sum(sum(col/i**2) for i, col in enumerate(frl_mat.transpose()))
+    sre_down = sum(sum(row) for row in frl_mat)
+    return sre_up / sre_down
